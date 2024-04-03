e5887a00cb343c7bc9289342aadf3e85a0a57a98
slne18 <solene.noize@epfl.ch>
2024-04-03 15:35:17 +0200

    Implement a simple interaction using Touch Controllers

--- Project/Assets/MainPlayerController.cs
+++ Project/Assets/MainPlayerController.cs
@@ -1,16 +1,3 @@
 using UnityEngine;
 
-public class MainPlayerController : MonoBehaviour
-{
-
-	/* ... */
-
-	void Start() { /* ... */ }
-
-	void Update()
-	{
-		if (OVRInput.Get(OVRInput.Button.One)) Debug.LogWarning("Button is pressed");
-	}
-
-	/* ... */
-}
+public class MainPlayerController : MonoBehaviour { }
